7c9a309bbf5d6258efad4a42776f723f343ae416
angelique-m08 <mora.angelique@gmail.com>
2020-07-22 15:02:59 +0200

    feat(sql): create booklib database

---
+++ booklib.sql
@@ -0,0 +1,51 @@
+DROP DATABASE IF EXISTS Booklib;
+
+CREATE DATABASE Booklib; 
+
+USE Booklib; 
+
+/* -------- Tables creation  --------- */
+
+CREATE TABLE `book` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `title` VARCHAR(255),
+  `resume` VARCHAR(255),
+  `cover` VARCHAR(255),
+  `genre` VARCHAR(255)
+);
+
+CREATE TABLE `author` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `firstname` VARCHAR(255),
+  `lastname` VARCHAR(255),
+  `biography` VARCHAR(255)
+);
+
+CREATE TABLE `review` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `review` VARCHAR(255),
+  `book_id` INT NOT NULL
+);
+
+CREATE TABLE `book_author` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `book_id` INT NOT NULL,
+  `author_id` INT NOT NULL
+);
+
+/* ------------ Contraints  ------------- */
+
+ALTER TABLE `review`
+ADD CONSTRAINT `fk_book_review`
+FOREIGN KEY (`book_id`)
+REFERENCES `book`(`id`);
+
+ALTER TABLE `book_author`
+ADD CONSTRAINT `fk_author_id`
+FOREIGN KEY (`author_id`)
+REFERENCES `author`(`id`);
+
+ALTER TABLE `book_author`
+ADD CONSTRAINT `fk_book_id`
+FOREIGN KEY (`book_id`)
+REFERENCES `book`(`id`);
