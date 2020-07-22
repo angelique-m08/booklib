9cfd51e28e4b4647a12375b02ffb7dba565f1ae0
angelique-m08 <mora.angelique@gmail.com>
2020-07-22 15:57:58 +0200

    feat(routes): create book file and route get

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const indexRouter = require('./routes');
 
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
+
+app.use('/', indexRouter);
 
 app.get('/', (request, response) => {
   response.json({message: 'Bienvenue sur Express'} );
