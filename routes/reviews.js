9f0cfc58267800b94e1efb13968e9b1573cd2efb
angelique-m08 <mora.angelique@gmail.com>
2020-07-23 11:25:20 +0200

    feat(review): create review file and get route

---
+++ routes/reviews.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const connection = require('../conf');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  connection.query('SELECT * from review', (err, results) => {
+    if (err) {
+      return (
+        res.sendStatus(500)
+      )
+    }
+    res.status(200).json(results);
+  });
+});
+
+module.exports = router;
